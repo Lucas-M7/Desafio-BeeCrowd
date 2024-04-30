7b31154a55b111ce06c0b73f2702028c4394a58c
Lucas Mateus <Lucas015x@gmail.com>
2024-04-30 18:50:48 -0300

    Calcular o raio de uma esfera

---
+++ Desafio-1/Program.cs
@@ -0,0 +1,7 @@
+﻿double divisao = 4 / 3.0;
+double pi = 3.14159;
+double raio = double.Parse(Console.ReadLine());
+
+double calculo = divisao * pi * Math.Pow(raio, 3);
+
+Console.WriteLine($"VOLUME = {calculo.ToString("F3")}");
